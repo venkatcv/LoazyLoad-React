e7bb21215f88c912a2b57c5f2f4f7e6e27d9487a
venkatcv <40715355+venkatcv@users.noreply.github.com>
2018-07-02 17:23:44 +0530

    Add files via upload

--- src/containers/LazyLoad.js
+++ src/containers/LazyLoad.js
@@ -5,24 +5,25 @@ import * as Source from '../components/DataSoruce';
 
 class LazyLoad extends Component {
     state ={
-        limit:10
+        limit:5
     }
     render(){
         return (
             <div>
             <Placeholder data={Source.Data} limit={this.state.limit}/>
-            <button onClick={() => {this.LoadDataHandler(this.state.limit)}}>Loda Data</button>
-            <button onClick={() => {this.Reset(10)}}>Reset</button>
+            <button class="btn btn-primary active" onClick={() => {this.LoadDataHandler(this.state.limit)}}>Load more results</button>
+			<span> &nbsp; </span>
+            <button class="btn btn-primary btn-info" onClick={() => {this.Reset(5)}}>Reset</button>
             </div>
         )
     }
 
     LoadDataHandler = (e) =>{
-      let  loadCount =  e+10;
+      let  loadCount =  e+6;
       this.setState({limit:loadCount});
     }
     Reset = (e) =>{
-      this.setState({limit:10});
+      this.setState({limit:5});
     }
 }
 
