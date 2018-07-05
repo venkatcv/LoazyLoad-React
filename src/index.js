eb45daa4c41bc6af705d3e6ed2c45f1c52088ec2
venkatcv <40715355+venkatcv@users.noreply.github.com>
2018-07-05 12:29:06 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+  import 'core-js/es6/map';
+  import 'core-js/es6/set';
+  import 'core-js/fn/array/find';
+  import 'core-js/fn/array/includes';
+  import 'core-js/fn/number/is-nan';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
